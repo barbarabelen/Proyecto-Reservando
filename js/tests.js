6361f988eccb0437d09295a89cecd219938cd3c1
Barbara Imperatori <barbarabelen.i@gmail.com>
2018-10-13 13:50:15 -0300

    test funcion obtenerPuntuacion casi terminado

--- js/tests.js
+++ js/tests.js
@@ -2,8 +2,8 @@
 
 let expect = chai.expect;
 
-describe ('Horario reservado', function(){
-    it('Se elimina del array', function(){
+describe('Horario reservado', function () {
+    it('Se elimina del array', function () {
         //elijo primer restaurante para testear
         const testHorarios = listado.restaurantes[0];
         //se reserva y elimina del array un horario
@@ -17,3 +17,28 @@ describe ('Horario reservado', function(){
 
 //test de la función obtenerPuntuacion()
 
+describe('Puntuaciones', function () {
+    it('Se ejecuta el promedio correctamente', function () {
+        let calificacionesPorDefault = listado.restaurantes[1].calificaciones;
+        let sumarCalificaciones = 0;
+        //tengo que guardar el for en la variable promedio
+        function promedio() {
+            for (let i = 0; i < calificacionesPorDefault.length; i++) {
+                let totalDeCalificaciones = sumarCalificaciones += calificacionesPorDefault[i];
+                totalDeCalificaciones / calificacionesPorDefault.length;
+            }
+        }
+
+        expect(calificacionesPorDefault.obtenerPuntuacion()).to.equal(promedio()); //no sé si apliqué bien el promedio() para la comparación
+    })
+
+    it('Deben dar 0 si no hay calificación', function () {
+        let listadoDeCalificaciones = listado.restaurantes[1].calificaciones;
+        //tengo que recorrer el listado e ir sacando calificaciones para que de 0
+        for (let i = listadoDeCalificaciones.length; i > 0; i--) {
+            listadoDeCalificaciones.pop();
+        }
+
+        expect(listadoDeCalificaciones.obtenerPuntuacion()).to.equal(0); 
+    })
+})
